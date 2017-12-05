f06ad29d816aa481d06eac34d1daff7d1c6453b7
仁一 伊藤 <150215@jc-21.jp>
2017-12-05 15:44:19 +0900

    計算　追加

--- RM/RM/MainPage.xaml.cs
+++ RM/RM/MainPage.xaml.cs
@@ -14,9 +14,62 @@ namespace RM
             InitializeComponent();
         }
 
+
         private void Button_Clicked(object sender, EventArgs e)
         {
+            Double w = Double.Parse(Weight.Text);
+            Double c = Double.Parse(Count.Text);
+            Double rm = Double.Parse(RM.Text);
+            
+            if(c == 1)
+            {
+                w / 1 = rm;
+            }
+            
+            else if(c == 2)
+            {
+                w / 0.95 = rm;
+            }
 
+            else if (c == 3)
+            {
+                w / 0.93 = rm;
+            }
+
+            else if (c == 4)
+            {
+                w / 0.9 = rm;
+            }
+
+            else if (c == 5)
+            {
+                w / 0.87 = rm;
+            }
+
+            else if (c == 6)
+            {
+                w / 0.85 = rm;
+            }
+
+            else if (c == 7)
+            {
+                w / 0.87 = rm;
+            }
+
+            else if (c == 8)
+            {
+                w / 0.8 = rm;
+            }
+
+            else if (c == 9)
+            {
+                w / 0.77 = rm;
+            }
+
+            else if (c == 10)
+            {
+                w / 0.70 = rm;
+            }
         }
     }
 }
