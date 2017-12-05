0d9e9eaacccf21aeaba5a70f7922ac50c0748bcd
仁一 伊藤 <150215@jc-21.jp>
2017-12-05 16:32:52 +0900

    int型にした

--- RM/RM/MainPage.xaml.cs
+++ RM/RM/MainPage.xaml.cs
@@ -16,7 +16,7 @@ namespace RM
        private void Button_Clicked(object sender, EventArgs e)
         {
             Double w = Double.Parse(Weight.Text);
-            Double c = Double.Parse(Count.Text);
+            int c = int.Parse(Count.Text);
             Double rm = Double.Parse(RM.Text);
             
             if(c == 1)
