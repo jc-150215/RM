017d481478a7d2b4444e152cc05bd680bc08297b
仁一 伊藤 <150215@jc-21.jp>
2017-12-06 12:20:56 +0900

    selecteditem 試し

--- RM/RM/MainPage.xaml.cs
+++ RM/RM/MainPage.xaml.cs
@@ -15,60 +15,7 @@ namespace RM
         }
        private void Button_Clicked(object sender, EventArgs e)
         {
-            Double w = Double.Parse(Weight.Text);
-            Double rm = Double.Parse(RM.Text);
-            c = Count.SelectedItem;
-            int c = int.Parse(c);
-            
-            if(c == 1)
-            {
-                w / 1 = rm;
-            }
-            
-            else if(c == 2)
-            {
-                w / 0.95 = rm;
-            }
-
-            else if (c == 3)
-            {
-                w / 0.93 = rm;
-            }
-
-            else if (c == 4)
-            {
-                w / 0.9 = rm;
-            }
-
-            else if (c == 5)
-            {
-                w / 0.87 = rm;
-            }
-
-            else if (c == 6)
-            {
-                w / 0.85 = rm;
-            }
-
-            else if (c == 7)
-            {
-                w / 0.87 = rm;
-            }
-
-            else if (c == 8)
-            {
-                w / 0.8 = rm;
-            }
-
-            else if (c == 9)
-            {
-                w / 0.77 = rm;
-            }
-
-            else
-             {
-                w / 0.70 = rm;
-            }
+            DisplayAlert(Count.SelectedItem);
         }
     }
 }
