1da1cdcc539bf601e4e2ee4f8ae06ced73e9f1c8
仁一 伊藤 <150215@jc-21.jp>
2017-12-06 16:26:03 +0900

    変更

--- RM/RM/MainPage.xaml.cs
+++ RM/RM/MainPage.xaml.cs
@@ -16,59 +16,60 @@ namespace RM
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            Double w = Double.Parse(Weight.Text);           
+            Double w = Double.Parse(Weight.Text);
+            Double r = Double.Parse(RM.Text);
             String s = (String)count.SelectedItem;
             int dCount = int.Parse(s);
 
 
             if (dCount == 1)
             {
-                RM.Text = w / 1;
+                r = w / 1;
             }
 
             else if (dCount == 2)
             {
-                RM.Text = w / 0.95;
+                r = w / 0.95;
             }
 
             else if (dCount == 3)
             {
-                RM.Text = w / 0.93;
+                r = w / 0.93;
             }
 
             else if (dCount == 4)
             {
-                RM.Text = w / 0.9;
+                r = w / 0.9;
             }
 
             else if (dCount == 5)
             {
-                RM.Text = w / 0.87;
+                r = w / 0.87;
             }
 
             else if (dCount == 6)
             {
-                RM.Text = w / 0.85;
+                r = w / 0.85;
             }
 
             else if (dCount == 7)
             {
-                RM.Text = w / 0.87;
+                r = w / 0.87;
             }
 
             else if (dCount == 8)
             {
-                RM.Text = w / 0.8 ;
+                r = w / 0.8 ;
             }
 
             else if (dCount == 9)
             {
-                RM.Text = w / 0.77;
+                r = w / 0.77;
             }
 
             else if(dCount == 10)
             {
-                RM.Text = w / 0.7;
+                r = w / 0.7;
             }
         }
     }
